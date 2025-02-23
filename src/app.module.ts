840a377d9cb53f270cde2bd6e24f45effd21d534
Paulo <zeronflash@gmail.com>
2025-02-23 21:29:18 +0100

    changed mysql configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,7 +14,7 @@ import { Saved } from './saved/entity/saved.entity';
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
+      host: process.env.DATABASE_HOST,
       port: 3306,
       username: process.env.DATABASE_USERNAME,
       password: process.env.DATABASE_PASSWORD,
